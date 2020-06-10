e5d00c078441506f0959507730de91764b3561a6
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-06-10 15:44:54 +0300

    Navigation (navigate, replace, push, pop)

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, FlatList } from 'react-native';
 
 const CategoriesScreen = props => {
     //console.log(props);
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -11,6 +11,14 @@ const CategoryMealScreen = props => {
                     props.navigation.navigate('MealDetail');
                 }}
             />
+
+            <Button 
+                title = "Go Back"
+                onPress = {() => {
+                    props.navigation.pop();
+                }}    
+            />
+
         </View>
     );
 };
