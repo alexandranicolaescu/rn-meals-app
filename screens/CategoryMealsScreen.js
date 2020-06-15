d7d2ebfe10152a6a63e29219edaddcbcd6a9dfff
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-06-15 18:14:03 +0300

    Filter Logic done

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { View, StyleSheet} from 'react-native';
 import { useSelector } from 'react-redux';
-
 
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
-
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealsScreen = props => {
     
@@ -15,6 +15,14 @@ const CategoryMealsScreen = props => {
     const displayedMeals = availableMeals.filter(
         meal => meal.categoryIds.indexOf(catId) >= 0
     );
+
+    if(displayedMeals.length === 0) {
+        return <View style = {styles.content}>
+            <DefaultText>
+                No meals found. Maybe check your filters?
+            </DefaultText>
+        </View>
+    }
     
     return (
         <MealList 
@@ -33,5 +41,12 @@ CategoryMealsScreen.navigationOptions = navigationData => {
     };
 };
 
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default CategoryMealsScreen;
